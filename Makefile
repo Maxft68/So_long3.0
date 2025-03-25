047523f26f71c33b485d8220bee87dd3f7282aeb
Max <mdsiurds@gmail.com>
2025-03-25 02:54:11 +0100

    before dodo

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: mdsiurds <mdsiurds@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/03/18 22:35:59 by mdsiurds          #+#    #+#              #
-#    Updated: 2025/03/18 22:58:47 by mdsiurds         ###   ########.fr        #
+#    Updated: 2025/03/24 21:55:14 by mdsiurds         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,15 +19,22 @@ CFLAGS = -Wall -Wextra -Werror -g3
 LIBFT_DIR = ./libft
 LIBFT_LIB = ./libft/libft.a
 
+# MLX42 configuration
+MLX42_DIR = ./MLX42
+MLX42_BUILD = $(MLX42_DIR)/build
+MLX42_LIB = $(MLX42_BUILD)/libmlx42.a
+MLX42_INC = -I$(MLX42_DIR)/include
+MLX42_FLAGS = -lXext -lm -lX11 -lglfw
+
 all: $(NAME)
 
-$(NAME): $(OBJ) $(LIBFT_LIB)
+$(NAME): $(OBJ) $(LIBFT_LIB) $(MLX42_LIB)
 		@echo "Compiling so_long..."
-		@$(CC) $(CFLAGS) $(OBJ) $(LIBFT_LIB) -o $(NAME)
+		@$(CC) $(CFLAGS) $(OBJ) $(LIBFT_LIB) $(MLX42_LIB) $(MLX42_FLAGS) -o $(NAME)
 		@echo "So_long successfully compiled.\n"
 
 $(OBJ_DIR)/%.o: %.c | $(OBJ_DIR)
-	@$(CC) $(CFLAGS) -c $< -o $@
+	@$(CC) $(CFLAGS) $(MLX42_INC) -c $< -o $@
 
 $(OBJ_DIR):
 	@mkdir -p $(OBJ_DIR)
@@ -36,10 +43,19 @@ $(LIBFT_LIB):
 	@echo	"Compiling libft..."
 	@make -C $(LIBFT_DIR) >/dev/null 2>&1
 	@echo "Libft successfully compiled."
+
+$(MLX42_LIB):
+	@echo "Compiling MLX42..."
+	@mkdir -p $(MLX42_BUILD)
+	@cd $(MLX42_DIR) && cmake -B build >/dev/null 2>&1
+	@cd $(MLX42_DIR) && cmake --build build -j4 >/dev/null 2>&1
+	@echo "MLX42 successfully compiled."
+
 clean:
 	@echo	"Cleaning..."
 	@rm -rf $(OBJ)
 	@make -C $(LIBFT_DIR) clean >/dev/null 2>&1
+	@rm -rf $(MLX42_BUILD)
 	@echo	"Successfully cleaned !\n"
 	@rm -rf $(OBJ_DIR)
 
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: mdsiurds <mdsiurds@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 22:46:41 by mdsiurds          #+#    #+#             */
-/*   Updated: 2025/03/21 18:35:08 by mdsiurds         ###   ########.fr       */
+/*   Updated: 2025/03/25 02:12:25 by mdsiurds         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "./libft/libft/libft.h"
 # include "./libft/printf/ft_printf.h"
 # include "./libft/gnl/get_next_line.h"
+# include "MLX42/MLX42.h"
 
 # include <fcntl.h>
 # include <stdio.h>
@@ -32,24 +33,33 @@ typedef struct s_image
 
 typedef struct s_game
 {
-	char	**map;
-	char	**map_to_check;
-	int		fd;
-	char	*name_map;
-	int		len;
-	int		first_len; // X
-	int		game_line; // Y
-	int		pos_x;
-	int		pos_y;
-	int		nb_rainbow;
-	int		nb_players;
-	int		nb_unicorn;
-	mlx
+	char			**map;
+	char			**map_to_check;
+	int				fd;
+	char			*name_map;
+	int				len;
+	int				first_len; // X
+	int				game_line; // Y
+	int				pos_x;
+	int				pos_y;
+	int				nb_rainbow;
+	int				nb_players;
+	int				nb_unicorn;
+	mlx_texture_t	*t_player;
+	mlx_image_t		*player;
+	mlx_texture_t	*t_rainbow;
+	mlx_image_t		*rainbow;
+	mlx_texture_t	*t_floor;
+	mlx_image_t		*floor;
+	mlx_texture_t	*t_wall;
+	mlx_image_t		*wall;
+	mlx_texture_t	*t_unicorn;
+	mlx_image_t		*unicorn;
+	mlx_t			*mlx;
 	
 	t_image	*image;
 	
 }			t_game;
-
 
 
 void	initialize(t_game *game, char **argv);
@@ -60,12 +70,19 @@ void	fill_the_map(t_game *game);
 void	free_array(char **array);
 void	close_all_array(t_game *game);
 void	print_map(char **map); // A SUPPRIMER A LA FIN
-void	ft_exit(char *error, t_game *game);
-void	verif_p_c_e(t_game *game);
-int		is_p_c_e(char c, t_game *game);
-void	verif_nb_p_c_e(t_game *game);
-void	verif_map_close(t_game *game);
-void	verif_all_valid_paths(t_game *game);
-void	verif_all_access(char **map, t_game *game);
+void	ft_exit(char *error, t_game *game, mlx_t *mlx);
+void	verif_p_c_e(t_game *game, mlx_t *mlx);
+int		is_p_c_e(char c, t_game *game, mlx_t *mlx);
+void	verif_nb_p_c_e(t_game *game, mlx_t *mlx);
+void	verif_map_close(t_game *game, mlx_t *mlx);
+void	verif_all_valid_paths(t_game *game, mlx_t *mlx);
+void	verif_all_access(char **map, t_game *game, mlx_t *mlx);
+void	verif_after_fill(t_game *game, mlx_t *mlx);
+void	begin_mlx(mlx_t *mlx, t_game *game);
+void	load_textures(mlx_t *mlx, t_game *game);
+void	initialize_mlx(t_game *game);
+void	load_images(mlx_t *mlx, t_game *game);
+void	load_to_windows(mlx_t *mlx, t_game *game);
+void	ft_hook(void *game);
 
 #endif
